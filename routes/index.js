e9741540a51eee2779b4cf4e0a659f4365b53aea
Simon Renblad <simon.renblad@outlook.com>
2020-12-23 19:36:47 +0800

    added controllers and middleware

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
-import express from "express";
-
-// controllers
-
-import users from "../controllers/user.js";
-
+import express from 'express';
 // middlewares
-
 import { encode } from '../middlewares/jwt.js';
 
 const router = express.Router();
 
 router
-    .post('/login/:userId', encode, (req, res, next) => {});
+  .post('/login/:userId', encode, (req, res, next) => {
+    return res
+      .status(200)
+      .json({
+        success: true,
+        authorization: req.authToken,
+      });
+  });
 
 export default router;
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,13 @@
 import express from 'express';
-
-//controllers
-
+// controllers
 import user from '../controllers/user.js';
 
 const router = express.Router();
 
 router
-    .get('/', user.onGetAllUsers)
-    .post('/', user.onCreateUser)
-    .get('/:id', user.onGetUserById)
-    .delete('/:id', user.onDeleteById)
+  .get('/', user.onGetAllUsers)
+  .post('/', user.onCreateUser)
+  .get('/:id', user.onGetUserById)
+  .delete('/:id', user.onDeleteUserById)
 
 export default router;
